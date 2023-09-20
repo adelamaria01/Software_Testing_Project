d31c07330ada0373bf19268338a88316ee13faff
Kovacs Adela-Maria <adelakovacs452@gmail.com>
2023-09-20 11:11:55 +0300

    Project

---
+++ GradeIt/EnVersion/Functii_FacultateCalcul.js
@@ -0,0 +1,113 @@
+function addRow() {
+    var buttonAdd = document.getElementById('btn-AddNota'),
+        parentElem = document.getElementById('parentElement'),
+        actualRows = document.getElementsByClassName('materieNota'),
+        rowCreated = document.createElement('tr'),
+        tdNr = document.createElement('td'),
+        tdMaterie = document.createElement('td'),
+        tdNota = document.createElement('td'),
+        tdCredite = document.createElement('td'),
+        inputMaterie = document.createElement('input'),
+        inputNota = document.createElement('input'),
+        inputCredite = document.createElement('input'),
+        nrCrtCalculat = actualRows.length + 1,
+        nrCrt = document.createTextNode(nrCrtCalculat),
+        nrCrtSpan = document.createTextNode(" " + nrCrtCalculat),
+        textMaterie = 'Subject ' + ' ...';
+
+    parentElem.appendChild(rowCreated);
+    rowCreated.setAttribute('class', 'materieNota');
+
+    rowCreated.appendChild(tdNr);
+    tdNr.appendChild(nrCrt);
+
+    rowCreated.appendChild(tdMaterie);
+    tdMaterie.appendChild(inputMaterie);
+    inputMaterie.setAttribute('type', 'text');
+    inputMaterie.setAttribute('placeholder', textMaterie);
+    inputMaterie.setAttribute('class', 'inputMaterie');
+
+    rowCreated.appendChild(tdNota);
+    tdNota.appendChild(inputNota);
+    inputNota.setAttribute('type', 'number');
+    inputNota.setAttribute('class', 'note inputNote');
+    inputNota.setAttribute('min', '5');
+    inputNota.setAttribute('max', '10');
+    inputNota.setAttribute('step', '1');
+    inputNota.setAttribute('placeholder', 'Grade...');
+
+    rowCreated.appendChild(tdCredite);
+    tdCredite.appendChild(inputCredite);
+    inputCredite.setAttribute('type', 'number');
+    inputCredite.setAttribute('class', 'credite inputNote');
+    inputCredite.setAttribute('min', '1');
+    inputCredite.setAttribute('max', '15');
+    inputCredite.setAttribute('step', '1');
+    inputCredite.setAttribute('placeholder', 'Număr');
+
+
+}
+
+function removeRow() {
+    var buttonRemove = document.getElementById('btn-RemoveNota'),
+        parentElem = document.getElementById('parentElement'),
+        materieNotaRow = document.getElementsByClassName('materieNota'),
+        lastRow = materieNotaRow.length - 1;
+
+    parentElem.removeChild(materieNotaRow[lastRow]);
+}
+
+
+
+function calculMedie() {
+    var containerCalcul = document.getElementById('dateInitiale'),
+        note = document.getElementsByClassName('note'),
+        credite = document.getElementsByClassName('credite'),
+        // outputMedie = document.querySelector('.container-rezultat .rezultat-final #rezultat'),
+        produsNotaCredite = 0,
+        sumaProduse = 0,
+        totalCredite = 0,
+        rezultatMedie = 0,
+        x = 1,
+        nonEmptyInputs = 0;
+
+
+    for (var i = 0; i < note.length; i++) {
+        if (note[i].value !== '') {
+            if ((note[i].value > 10 || note[i].value < 1)) {
+                alert("GRADES must be between 1 and 10!");
+                location.reload();
+                x = 0;
+                break;
+            }
+
+            if (credite[i].value > 10 || credite[i].value < 1) {
+                alert("CREDITS must be between 1 and 10!");
+                location.reload();
+                x = 0;
+                break;
+            }
+            produsNotaCredite = Number(note[i].value) * Number(credite[i].value);
+            sumaProduse += produsNotaCredite;
+            totalCredite += Number(credite[i].value);
+            rezultatMedie = sumaProduse / totalCredite;
+            document.getElementById("rezultat").innerHTML = toFixed(rezultatMedie, 2);
+
+            nonEmptyInputs++;
+        }
+
+    }
+
+    if (x == 1) {
+        rezultatMedie = sumaProduse / totalCredite;
+        document.getElementById("rezultat").innerHTML = toFixed(rezultatMedie, 2);
+    }
+
+
+
+}
+
+function toFixed(num, fixed) {
+    var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+    return num.toString().match(re)[0];
+}
---
+++ GradeIt/RoVersion/Functii_LiceuCalcul.js
@@ -0,0 +1,89 @@
+function addRow() {
+    var buttonAdd = document.getElementById('btn-AddNota'),
+        parentElem = document.getElementById('parentElement'),
+        actualRows = document.getElementsByClassName('materieNota'),
+        rowCreated = document.createElement('tr'), // Creaza tagul tr
+        tdNr = document.createElement('td'), // creaza tagul td pentru Nr
+        tdMaterie = document.createElement('td'),  // creaza tagul td pentru Materie
+        tdNota = document.createElement('td'), // creaza tagul td pentru Nota
+        inputMaterie = document.createElement('input'),
+        inputEl = document.createElement('input'),
+        nrCrtCalculat = actualRows.length + 1,
+        nrCrt = document.createTextNode(nrCrtCalculat), // numarul pentru tagul td pentru numar crt
+        // nrCrtInputMaterie = document.createTextNode(" " + nrCrtCalculat),
+        textMaterie = 'Materie ' + ' ...';
+
+    parentElem.appendChild(rowCreated); //Ataseaza randul la elementul parinte tbody
+    rowCreated.setAttribute('class', 'materieNota'); //Adauga attributul si clasa
+
+    rowCreated.appendChild(tdNr); // Adauga tagul td la randul creat tr
+    tdNr.appendChild(nrCrt); // Adauga textul la tagul td
+
+    rowCreated.appendChild(tdMaterie); // Adauga tagul td pentru Materie
+    //tdMaterie.appendChild(textMaterie); // Adauga textul la tagul creat
+    tdMaterie.appendChild(inputMaterie); // Adauga tagul input in tagul td
+    inputMaterie.setAttribute('type', 'text'); // Adauga attribut la input
+    inputMaterie.setAttribute('placeholder', textMaterie); // Adauga attribut la input
+    inputMaterie.setAttribute('class', 'inputMaterie'); // Adauga attribut la input
+
+
+    rowCreated.appendChild(tdNota);
+    tdNota.appendChild(inputEl);
+    inputEl.setAttribute('type', 'number');
+    inputEl.setAttribute('class', 'note inputNote');
+    inputEl.setAttribute('min', '5');
+    inputEl.setAttribute('max', '10');
+    inputEl.setAttribute('placeholder', 'Nota...');
+    inputEl.setAttribute('style', 'width:150px');
+
+}
+
+
+function removeRow() {
+    var buttonRemove = document.getElementById('btn-RemoveNota'),
+        parentElem = document.getElementById('parentElement'),
+        materieNotaRow = document.getElementsByClassName('materieNota'),
+        lastRow = materieNotaRow.length - 1;
+
+    parentElem.removeChild(materieNotaRow[lastRow]);
+}
+
+
+function calculMedie() {
+    var containerCalcul = document.getElementById('dateInitiale'),
+        note = document.getElementsByClassName('note'),
+        sumaNote = 0,
+        rezultatMedie = 0;
+        x = 1;
+  
+    var nonEmptyInputs = 0;
+  
+    for (var i = 0; i < note.length; i++) {
+      if (note[i].value !== '') {
+        if(note[i].value > 10 || note[i].value < 1) {
+            alert("Toate notele trebuie sa fie intre 1 si 10!");
+            location.reload();
+            x = 0;
+            break;
+        }
+        sumaNote += Number(note[i].value);
+        nonEmptyInputs++;
+      }
+    }
+  
+    if(x == 1) {
+      rezultatMedie = sumaNote / nonEmptyInputs;
+      document.getElementById("rezultat").innerHTML = toFixed(rezultatMedie, 2);
+    }
+  }
+
+
+function toFixed(num, fixed) {
+    var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+    return num.toString().match(re)[0];
+}
+
+
+
+
+
